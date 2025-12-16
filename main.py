8915afc3db14f580610ba7987754e5c2f4cb064b
Sidharth-prog-eng <sidharthsidhu7005@gmail.com>
2025-12-16 03:35:06 +0000

    put validations

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-def main():
-    name = input("Please enter your name: ").strip()
+def get_name() -> str:
+    name = input("Please enter your name: ")
+    return name
 
+
+def validate_name(name: str) -> bool:
     if not name:
-        print("invalid name")
-        return
+        return False
+    return name.isalpha()
 
-    print({"message":f"Hello, {name}!"})
+def build_response(name: str) -> dict:
+    return {"message" : f"Hello {name}"}
+
+def build_error(message: str) -> dict:
+    return {"error": message}
+
+def main():
+    name = get_name()
+
+    if not validate_name(name):
+        print(build_error("Invalid name. Only alphabetic characters allowed."))
+
+        return
+    response = build_response(name)
+    print(response)
 
 if __name__ == "__main__":
     main()
