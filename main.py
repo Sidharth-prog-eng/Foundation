2acf2e665ab0f5a42f73812dd328a3026daf4411
Sidharth-prog-eng <sidharthsidhu7005@gmail.com>
2025-12-16 04:06:21 +0000

    used custom exception for input validations

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+class InvalidNameError(Exception):
+    pass
+
 def get_name() -> str:
     name = input("Please enter your name: ")
     return name
@@ -5,8 +8,11 @@ def get_name() -> str:
 
 def validate_name(name: str) -> bool:
     if not name:
-        return False
-    return name.isalpha()
+        raise InvalidNameError("Name cannot be empty.")
+
+    if not name.isalpha():
+        raise InvalidNameError("Name must be alphabetic.")
+
 
 def build_response(name: str) -> dict:
     return {"message" : f"Hello {name}"}
@@ -15,14 +21,12 @@ def build_error(message: str) -> dict:
     return {"error": message}
 
 def main():
-    name = get_name()
-
-    if not validate_name(name):
-        print(build_error("Invalid name. Only alphabetic characters allowed."))
-
-        return
-    response = build_response(name)
-    print(response)
+    try:
+        name = get_name()
+        validate_name(name)
+        print(build_response(name))
+    except InvalidNameError as e:
+        print(build_error(str(e)))
 
 if __name__ == "__main__":
     main()
